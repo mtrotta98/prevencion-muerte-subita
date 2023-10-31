14459cdeeddc59324e1ebf12b60d58fefdcd8215
Elias Zaccheo <eliaszaccheo91@gmail.com>
2023-10-31 17:29:05 -0300

    Listado de DEAS

--- app/src/core/deas/__init__.py
+++ app/src/core/deas/__init__.py
@@ -3,7 +3,7 @@ from src.core.db import db
 
 def get_all():
     """Esta funcion devuelve todos los DEAS"""
-    return DEA.query.all()
+    return DEA.query.order_by(db.asc(DEA.id)).all()
 
 def get_by_id(id):
     """Retorna un DEA en específico"""
@@ -11,12 +11,18 @@ def get_by_id(id):
 
 def get_by_sede(id_sede):
     """Retorna los DEA de una sede"""
-    return DEA.query.filter_by(sede_id=id_sede).all()
+    return DEA.query.filter_by(sede_id=id_sede).order_by(db.asc(DEA.id)).all()
 
 def save(self):
     """ Salva los cambios"""
     db.session.add(self)
     db.session.commit()
+
+def deactivate(dea):
+    """Desactiva el DEA"""
+    dea.activo = False
+    save(dea)
+
 
 @staticmethod
 def destroy(dea):
--- app/src/web/controllers/deas.py
+++ app/src/web/controllers/deas.py
@@ -3,13 +3,21 @@ from flask import Blueprint, render_template, request, flash, redirect, abort
 from flask import url_for
 from src.web.controllers.validators.validator_permission import has_permission
 from src.core import deas
-from src.core import responsables
 from src.core import sedes
-from src.core import usuarios
 from src.web.controllers.forms.newDea import NewDEAForm
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
 dea_blueprint = Blueprint("deas", __name__, url_prefix="/deas")
+
+@dea_blueprint.get("/list/<sede_id>")
+@jwt_required()
+def dea_list(sede_id):
+    usuario_actual = get_jwt_identity()
+    if not (has_permission(usuario_actual, "representante_alta_dea")):
+        return abort(403)
+    dea_list=deas.get_by_sede(sede_id)
+    sede=sedes.get_sede(sede_id)
+    return render_template("deas/lista_deas_sede.html", deas=dea_list, sede=sede)
 
 @dea_blueprint.get("/new/<sede_id>")
 @jwt_required()
@@ -18,22 +26,6 @@ def dea_new(sede_id):
     if not (has_permission(usuario_actual, "representante_alta_dea")):
         return abort(403)
     newDea = NewDEAForm();
-    """
-    newDea.marca.choices =[('1', 'Meditech'),               # Reemplazar por API
-                                 ('2', 'PhisioControl'), 
-                                 ('3', 'Medtronic'), 
-                                 ('4', 'Mindray'), 
-                                 ('5', 'HeartSine'), 
-                                 ('6', 'Philips'), 
-                                 ('7', 'Zoll'), 
-                                 ('8', 'Schiller'), 
-                                 ('9', 'Defibtech')]"""
-    """
-    newDea.modelo.choices =[('1', 'Mempeliece'),            # Reemplazar por API
-                                 ('2', 'Accilingul'), 
-                                 ('3', 'Venerthagn'), 
-                                 ('4', 'Tamuligusa'), 
-                                 ('5', 'Flaucebato')]"""
     return render_template("deas/new.html",form=newDea, sede_id=sede_id)
 
 @dea_blueprint.route("/add", methods=['POST'])
@@ -50,9 +42,6 @@ def dea_create(sede_id):
         dea.sede_id = sede_id        
         # Intentar salvar
         try:
-            """resp = []
-            resp.append(responsables.get_by_id(form.__dict__['responsable'].__dict__['data']))
-            dea.responsables = resp"""
             deas.save(dea)        
             flash("Se creo correctamente el DEA: "+dea.denominacion, "success")
         except:
@@ -110,4 +99,20 @@ def dea_edit(id, **kwargs):
             for error in form.errors[field]:
                 flash(error, "error")
         return redirect(url_for("deas.dea_mod"))
-    return redirect(url_for('usuarios.inicio'))
+    return dea_list(dea.sede_id)
+    
+
+@dea_blueprint.get("/delete/<id>")
+@jwt_required()
+def dea_delete(id):
+    """Elimina un DEA, si este existe (baja logica, lo desactiva)"""
+    usuario_actual = get_jwt_identity()
+    if not (has_permission(usuario_actual, "representante_alta_dea")):
+        return abort(403)
+    dea=deas.get_by_id(id)
+    sede=dea.sede_id
+    if dea:
+        #deas.destroy(dea);
+        deas.deactivate(dea);
+    return dea_list(sede)
+
