21725302fa5a55c98548b28661855966b350048c
Nicolas Pierini <nicolasepierini@gmail.com>
2023-10-31 19:13:24 -0300

    PDF de sede y deas finalizado

--- app/src/web/controllers/exportaciones.py
+++ app/src/web/controllers/exportaciones.py
@@ -7,33 +7,31 @@ from io import BytesIO
 from flask_jwt_extended import jwt_required, unset_jwt_cookies
 from flask_jwt_extended import create_access_token, set_access_cookies, get_jwt_identity
 
-from src.core import usuarios
 from src.core import sedes
+from src.core import deas
+
 from src.web.controllers.validators import validator_usuario, validator_permission
 
 exportacion_blueprint = Blueprint("exportaciones", __name__, url_prefix="/exportaciones")
-
-@exportacion_blueprint.route("/tipo_exportacion/<id>")
-def devolver_dato(id):
-    """Esta funcion recibe el parametro de la lista que se quiere exportar en PDF"""
-
-    sede = sedes.get_sede(id)
-
-    return sede
 
 @exportacion_blueprint.route("/exportar_pdf/<id>")
 def generar_pdf(id):
     """Esta funcion genera el pdf, recibe por parametro el id de la sede"""
 
     # Obtener los datos
-    sede = devolver_dato(id)
+    sede = sedes.get_sede(id)
+    deas_sede = deas.get_by_sede(id)
 
     # Crear un objeto PDF
     output = BytesIO()
     p = canvas.Canvas(output, pagesize=letter)
 
     # Agregar datos al PDF
-    texto = f'Nombre: {sede.nombre}, Latitud: {sede.latitud}, Longitud: {sede.longitud}, Flujo de Personas: {sede.flujo_personas}, Personal: {sede.personal_estable}, Piso: {sede.pisos}, Estado: {sede.estado}'
+    texto = f'Nombre: {sede.nombre}, Latitud: {sede.latitud}, Longitud: {sede.longitud}, Flujo de Personas: {sede.flujo_personas}, Personal: {sede.personal_estable}, Piso: {sede.pisos}, Estado: {sede.estado}, DEAS de la Sede:'
+    
+    for dea in deas_sede:
+        texto += f',Denominacion: {dea.denominacion}, Marca: {dea.marca}, Modelo: {dea.modelo}, Nro. de Serie: {dea.nSerie}, Ultimo Mantenimiento: {dea.ultimoMantenimiento}, Solidario: {dea.solidario}, Activo: {dea.activo}'
+    
     lineas = texto.split(', ')
 
     for i, linea in enumerate(lineas):
